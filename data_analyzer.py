326e2a3fae75d6ea7465671fa1be84994ea715cf
seobin <qkdtjqls@naver.com>
2025-04-28 15:15:42 +0900

    update

--- data_analyzer.py
+++ data_analyzer.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import datetime
+import matplotlib.ticker as ticker 
 
 
 class DataAnalyzer:
@@ -26,29 +27,44 @@ class DataAnalyzer:
 
         print(f"Message field: {self.msg_field} | Shape: {self.df.shape} | {missing_info}")
 
-    def plot_feature_trend(self, feature: str, ax=None):
+    
+    def plot_feature_trend(self, feature: str, ax=None, sample_rate=10):
         if self.df is None:
             raise ValueError("Data is not loaded. Please run load(filepath) first.")
 
+        # 정상/이상 데이터 분리
         normal = self.df[self.df[self.label_col] == 0]
         abnormal = self.df[self.df[self.label_col] == 1]
 
+        # 샘플링
+        normal = normal.iloc[::sample_rate]
+        abnormal = abnormal.iloc[::sample_rate]
+
+        # x축: 각각 길이 맞춰 0부터
+        x_normal = range(len(normal))
+        x_abnormal = range(len(abnormal))
+
         if ax is None:
-            plt.plot(normal.index, normal[feature], color='blue', label='Normal')
-            plt.plot(abnormal.index, abnormal[feature], color='red', label='Abnormal')
-            plt.title(f'Feature Value Trend by Sequence : {feature}')
-            plt.xlabel('Index')
-            plt.ylabel(f'{feature} Value')
-            plt.legend()
-            plt.grid(True)
+            fig, ax = plt.subplots(figsize=(8, 4))
+
+        # 정상 plot
+        ax.plot(x_normal, normal[feature], color='blue', alpha=0.7, linewidth=0.7, label='Normal')
+
+        # 이상 plot
+        ax.plot(x_abnormal, abnormal[feature], color='red', alpha=0.8, linewidth=0.7, label='Abnormal')
+
+        ax.xaxis.set_major_locator(ticker.MaxNLocator(nbins=5))
+        ax.set_title(f'{feature}', fontsize=16)
+        ax.set_xlabel('Sample Index')
+        ax.set_ylabel(f'{feature} Value')
+        ax.grid(True)
+        ax.legend()
+
+        if ax is None:
+            plt.tight_layout()
             plt.show()
-        else:
-            ax.plot(normal.index, normal[feature], color='blue', label='Normal')
-            ax.plot(abnormal.index, abnormal[feature], color='red', label='Abnormal')
-            ax.set_title(f'{feature}')
-            ax.set_xlabel('Index')
-            ax.set_ylabel(f'{feature} Value')
-            ax.grid(True)
+
+
 
     def plot(self, save_dir="plots"):
         if self.df is None:
@@ -60,8 +76,13 @@ class DataAnalyzer:
             return
 
         n = len(features)
-        ncols = 4
-        nrows = (n + ncols - 1) // ncols
+        if n <= 4:
+            ncols = n
+            nrows = 1
+
+        else:
+            ncols = 4
+            nrows = (n + ncols - 1) // ncols
 
         fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(5 * ncols, 4 * nrows))
         axes = axes.flatten()
@@ -84,105 +105,9 @@ class DataAnalyzer:
         filename = f"{self.msg_field}.png"
         save_path = os.path.join(save_dir, filename)
 
-        plt.savefig(save_path, dpi=300)
+        plt.savefig(save_path, dpi=300, bbox_inches='tight')
         plt.show()
         plt.close(fig)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def load_data(filepath: str):
-#     df = pd.read_csv(filepath)
-#     df = df.drop(columns=['timestamp', 'TimeUS'], errors='ignore')
-#     return df
-
-# class Preprocessor:
-#     def __init__(self, filepath: str):
-#         self.name = os.path.basename(filepath).split("_")[0]
-#         self.df = load_data(filepath)
-#         self.preprocess()
-
-#     def preprocess(self):
-#         missing = self.df.isnull().sum()
-#         missing_cols = missing[missing > 0]
-
-#         if missing_cols.empty:
-#             missing_info = "No missing columns"
-#         else:
-#             missing_info = f"Missing columns ({len(missing_cols)}): {list(missing_cols.index)}"
-
-#         print(f"Message field: {self.name} | Shape: {self.df.shape} | {missing_info}")
-
-#     def to_analyzer(self, label_col='label'):
-#         return DataAnalyzer(self.df, label_col=label_col, msg_field=self.name)
-
-
-
-# class DataAnalyzer:
-#     def __init__(self, df, label_col='label', msg_field):
-#         self.df = df
-#         self.label_col = label_col
-#         self.msg_field = msg_field
-
-#     def plot_feature_trend(self, feature: str, ax=None):
-#         normal = self.df[self.df[self.label_col] == 0][feature]
-#         abnormal = self.df[self.df[self.label_col] == 1][feature]
-
-#         if ax is None:
-#             plt.plot(normal.index, normal[feature], color='blue', label='Normal')
-#             plt.plot(abnormal.index, abnormal[feature], color='red', label='Abnormal')
-#             plt.title(f'Feature Value Trend by Sequence : {feature}')
-#             plt.xlabel('Index')
-#             plt.ylabel(f'{feature} Value')
-#             plt.legend()
-#             plt.grid(True)
-#             plt.show()
-        
-#         else:
-#             ax.plot(normal.index, normal[feature], color='blue', label='Normal')
-#             ax.plot(abnormal.index, abnormal[feature], color='red', label='Abnormal')
-#             ax.set_title(f'{feature}')
-#             ax.set_xlabel('Index')
-#             ax.set_ylabel(f'{feature} Value')
-#             ax.grid(True)
-
-#     def plot_fields(self, bins=50):
-#         features = [col for col in self.df.columns if col != self.label_col]
-
-#         if not features:
-#             print("No features to plot.")
-#             return
-        
-#         n = len(features)
-#         if n <= 4:
-#             ncols = n
-#             nrows = 1
-
-#         else:
-#             ncols = 4
-#             nrows = (n + 1) // ncols
-
-#         fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(5 * ncols, 4 * nrows))
-#         axes = axes.flatten()
-
-#         for i, feature in enumerate(features):
-#             self.plot_feature_trend(feature, ax=axes[i])
-
-#         for j in range(i + 1, len(axes)):
-#             fig.delaxes(axes[j])  # 남는 빈 plot 삭제
-
-#         fig.suptitle(f'Distribution of Each Feature {self.msg_field}', fontsize=16)
-#         plt.tight_layout()
-#         plt.show()
